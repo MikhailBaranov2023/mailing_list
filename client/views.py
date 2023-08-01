48d66180ef933e71e2937d270f918149c3eff085
problemaprod <problemaprod@gmail.com>
2023-08-01 23:10:06 +0100

    создал проект описал модель Client и реализовал CRUD.

---
+++ client/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView, DetailView
+from client.models import Client
+from django.urls import reverse_lazy, reverse
+
+
+class ClientListView(ListView):
+    model = Client
+
+
+class ClientCreateView(CreateView):
+    model = Client
+    fields = ('name', 'email', 'comment')
+    success_url = reverse_lazy('client:list')
+
+
+class ClientUpdateView(UpdateView):
+    model = Client
+    fields = ('name', 'email', 'comment')
+    success_url = reverse_lazy('client:list')
+
+
+class ClientDetailView(DetailView):
+    model = Client
+
+
+class ClientDeleteView(DeleteView):
+    model = Client
+    success_url = reverse_lazy('client:list')
